75a7f2df6ecda4bf792570afc1f5e24c525bf97d
Felippe Rodrigo Puhle <felippe@dblinks.com.br>
2016-10-07 11:34:12 -0300

    Add react-router-bootstrap

--- src/components/admin/template/Sidebar.js
+++ src/components/admin/template/Sidebar.js
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 
+import { IndexLinkContainer, LinkContainer } from 'react-router-bootstrap';
 import { Nav, NavItem } from 'react-bootstrap';
 
 class Sidebar extends Component {
 
-  constructor() {
-    super();
-
-    this.state = {
-      activeKey: 1
-    }
-  }
-
   render() {
   	return (
       <aside>
-        <Nav bsStyle="pills" stacked activeKey={this.state.activeKey}>
-          <NavItem eventKey={1} href="/">Home</NavItem>
-          <NavItem eventKey={2} href="/groups">Groups</NavItem>
-          <NavItem eventKey={3} href="/users">Users</NavItem>
+        <Nav bsStyle="pills" stacked>
+          <IndexLinkContainer to={{ pathname: '/' }}>
+            <NavItem eventKey={1}>Home</NavItem>
+          </IndexLinkContainer>
+
+          <LinkContainer to={{ pathname: '/groups' }}>
+            <NavItem eventKey={2}>Groups</NavItem>
+          </LinkContainer>
+
+          <LinkContainer to={{ pathname: '/users' }}>
+            <NavItem eventKey={3}>Users</NavItem>
+            </LinkContainer>
         </Nav>
       </aside>
     )
